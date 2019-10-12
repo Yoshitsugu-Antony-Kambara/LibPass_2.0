c57821870a5eb85973b544c8f57c4db0ffd6a9ac
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-10-13 07:26:43 +0900

    a

---
+++ LibPass_2.0/testViewController.swift
@@ -0,0 +1,30 @@
+//
+//  testViewController.swift
+//  LibPass_2.0
+//
+//  Created by 神原良継 on 2019/10/13.
+//  Copyright © 2019 jp.Antony. All rights reserved.
+//
+
+import UIKit
+
+class testViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
