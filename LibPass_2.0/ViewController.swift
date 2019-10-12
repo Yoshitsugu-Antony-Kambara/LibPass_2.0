b429d19597b4b9eedea9ef1090f137520bcf6b18
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-10-13 00:59:39 +0900

    Initial Commit

---
+++ LibPass_2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LibPass_2.0
+//
+//  Created by 神原良継 on 2019/10/13.
+//  Copyright © 2019 jp.Antony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
