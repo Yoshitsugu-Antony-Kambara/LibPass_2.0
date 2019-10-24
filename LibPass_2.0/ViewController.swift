f041795bbfeb8ecef7c254e55f1735f052e56a0b
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-10-24 17:11:44 +0900

    テーブル実装

--- LibPass_2.0/ViewController.swift
+++ LibPass_2.0/ViewController.swift
@@ -9,42 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var sum: Int = 26500
+    var sum: Int = 0
     
     
     var nmArray: [Int] = [Int]()
     
     @IBOutlet var sumLabel: UILabel!
-    //@IBOutlet var table: UITableView!
+    @IBOutlet var table: UITableView!
     
-    //var bokArray:[[String]] = [[String]]()
-    
-    
+    var bokArray:[[String]] = [[String]]()
     
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        sumLabel.text = "26500"
-//        
-//        //table.delegate = self
-//        //table.dataSource = self
-//        
-//        //self.table.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
-////        for i in 0...nmArray.count {
-////            sum = sum + nmArray[i]
-////        }
-////
-//        //sumLabel.text = String(sum)
-//        
-//    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sumLabel.text = "0"
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        self.table.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
+        sumLabel.text = String(sum)
+    }
     
     
     override func viewWillAppear(_ animated: Bool) {
         for i in 0..<nmArray.count {
             sum = sum + nmArray[i]
         }
-        
         sumLabel.text = String(sum)
     }
     
@@ -52,36 +46,28 @@ class ViewController: UIViewController {
     @IBAction func scanButton() {
         let storyboard = self.storyboard!
         self.present(storyboard.instantiateViewController(withIdentifier: "ScanVC"), animated:true, completion: nil)
-        
     }
-    
 }
 
-//extension ViewController: UITableViewDataSource, UITableViewDelegate {
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//        return nmArray.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
-//
-//
-//        cell.name.text = String(self.nmArray[indexPath.row])
-//
-//
-//
-//
-//
-//        return cell
-//    }
-//}
 
+/*テーブルビューの設定*/
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return nmArray.count
+    }
     
     
-    
-    
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
+        cell.price.text = String(self.nmArray[indexPath.row])
+        
+        return cell
+    }
+}
+
+
     
 
